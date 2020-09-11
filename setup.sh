c9a15688453657ede4b4e332ad726d8dfbd087c2
ketank-new <ketan22584@gmail.com>
2020-09-11 15:10:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/quick-start-postgres.yaml
+kubectl create ns argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/quick-start-postgres.yaml
 curl -sLO https://github.com/argoproj/argo/releases/download/v2.11.0-rc2/argo-linux-amd64.gz
 gunzip argo-linux-amd64.gz
 chmod +x argo-linux-amd64
